17e0866e75fe6533e4f457c62a91628ef95492b5
NDA_ADN <nguyendinhan97@gmail.com>
2018-06-05 15:25:46 +0700

    nearly finish chapter 6

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: {maximum: Settings.name_max_length}
+  validates :email, presence: true, length: {maximum: Settings.email_max_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: Settings.pass_min_length}
+
+  before_save{email.downcase!}
+
+  has_secure_password
+end
